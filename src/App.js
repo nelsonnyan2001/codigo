32aa44a731bce0ec9eec38a9376915f89e03e475
nelsonnyan2001 <zwenyantoe@my.jcu.edu.au>
2020-12-25 22:17:44 +0630

    Fixed

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ export default class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      isLoading: false,
+      isLoading: true,
       currentActive: 0,
       menuOpen: false,
       currentFilter: '',
